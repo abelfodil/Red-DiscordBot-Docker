4aaf8a3f582996a978359c43ff3f274702a1af1c
Anes Belfodil <ans.belfodil@gmail.com>
2019-10-18 16:20:48 -0400

    Remove publish script and always build image locally

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-TAG=docker.pkg.github.com/abelfodil/red-discordbot-docker/red:latest
-
-docker build -t red .
-docker tag red $TAG
-docker push $TAG
